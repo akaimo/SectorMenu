3b3b0f5bdaf8c76f08cec505ca421a9722eb0323
shuhei <kawashu0723@gmail.com>
2015-09-23 13:00:18 +0900

    add sectorMenu file's

---
+++ SectorMenu/SectorMenu.swift
@@ -0,0 +1,21 @@
+//
+//  SectorMenu.swift
+//  SectorMenu
+//
+//  Created by akaimo on 2015/09/23.
+//  Copyright © 2015年 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class SectorMenu: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SectorMenu/SectorMenuItem.swift
@@ -0,0 +1,21 @@
+//
+//  SectorMenuItem.swift
+//  SectorMenu
+//
+//  Created by akaimo on 2015/09/23.
+//  Copyright © 2015年 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class SectorMenuItem: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
