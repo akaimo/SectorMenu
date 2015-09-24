4cbe36dd4e3bad28ac505a7879d0453a9744b330
shuhei <kawashu0723@gmail.com>
2015-09-24 21:30:34 +0900

    reset

--- SectorMenu/ViewController.swift
+++ SectorMenu/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let floatingFrame = CGRect(x: self.view.frame.width - 56 - 16, y: self.view.frame.height - 56 - 16, width: 56, height: 56)
+        let floatingActionButton = SectorMenu(frame: floatingFrame)
+        self.view.addSubview(floatingActionButton)
     }
 
     override func didReceiveMemoryWarning() {
