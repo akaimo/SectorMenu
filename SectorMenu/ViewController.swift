3a1e42bd272f8b3ccda57e400ce4705376a852f1
shuhei <kawashu0723@gmail.com>
2015-09-28 22:23:03 +0900

    fix sample

--- SectorMenu/ViewController.swift
+++ SectorMenu/ViewController.swift
@@ -12,20 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-//        let place = UIImage(named: "Place")
-//        let starMenuItem1: SectorMenuItem = SectorMenuItem(image: place)
-//        starMenuItem1.frame = CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0)
-//        starMenuItem1.backgroundColor = UIColor.blueColor()
-//        
-//        self.view.addSubview(starMenuItem1)
-        
-        let circle: SectorMenuCircle = SectorMenuCircle()
-        circle.radius = 25
-        circle.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        let circle: SectorMenuCircle = SectorMenuCircle(center: CGPoint(x: 100, y: 100), radius: 25, color: UIColor.blueColor())
         let img = UIImageView(image: UIImage(named: "Place"))
-        img.frame = CGRect(x: 12.5, y: 12.5, width: 25, height: 25)
+        img.frame = CGRect(x: 10, y: 10, width: 30, height: 30)
         circle.addSubview(img)
         self.view.addSubview(circle)
     }
