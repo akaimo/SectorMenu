9a4809003b8e5f497a14b5fc7a9626f40eca0a9b
shuhei <kawashu0723@gmail.com>
2015-09-28 17:56:01 +0900

    add test data

--- SectorMenu/ViewController.swift
+++ SectorMenu/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let place = UIImage(named: "Place")
+        let starMenuItem1: SectorMenuItem = SectorMenuItem(image: place)
+        starMenuItem1.frame = CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0)
+        starMenuItem1.backgroundColor = UIColor.blueColor()
+        
+        self.view.addSubview(starMenuItem1)
     }
 
     override func didReceiveMemoryWarning() {
