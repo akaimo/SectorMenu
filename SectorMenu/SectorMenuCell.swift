a38c5f293499bd8e87c90510000dc285b25e12cd
shuhei <kawashu0723@gmail.com>
2015-09-29 13:15:25 +0900

    add SectorMenuCell

---
+++ SectorMenu/SectorMenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  SectorMenuCell.swift
+//  SectorMenu
+//
+//  Created by akaimo on 2015/09/29.
+//  Copyright © 2015年 akaimo. All rights reserved.
+//
+
+import UIKit
+
+class SectorMenuCell: SectorMenuCircle {
+
+}
